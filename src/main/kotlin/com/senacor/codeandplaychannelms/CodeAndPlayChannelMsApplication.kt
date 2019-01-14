4486b94e6fbef7645d73180d66cdf7075004f097
Müller <rmueller1@MUERO-AMBP.local>
2019-01-14 08:55:28 +0100

    Initial commit

---
+++ src/main/kotlin/com/senacor/codeandplaychannelms/CodeAndPlayChannelMsApplication.kt
@@ -0,0 +1,12 @@
+package com.senacor.codeandplaychannelms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeAndPlayChannelMsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeAndPlayChannelMsApplication>(*args)
+}
+
